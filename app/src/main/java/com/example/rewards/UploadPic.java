90c0c592a7b7d7defcea1ad24e0aeefb51181ee7
ElvinaPau <elvina251003@gmail.com>
2023-12-29 21:23:16 +0800

    upload pic

--- app/src/main/java/com/example/rewards/UploadPic.java
+++ app/src/main/java/com/example/rewards/UploadPic.java
@@ -12,10 +12,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.firebase.FirebaseApp;
@@ -30,6 +33,8 @@ import java.util.UUID;
 
 public class UploadPic extends AppCompatActivity {
 
+    EditText name, description, point;
+    String name1, description1, point1;
     private StorageReference storageReference;
     private LinearProgressIndicator progress;
     private Uri image;
@@ -39,18 +44,15 @@ public class UploadPic extends AppCompatActivity {
     FirebaseDatabase database;
 
     private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK) {
-                        if (result.getData() != null) {
-                            image = result.getData().getData();
-                            uploadImage.setEnabled(true);
-                            Glide.with(getApplicationContext()).load(image).into(imageView);
-                        }
-                    } else {
-                        Toast.makeText(UploadPic.this, "Please select an image", Toast.LENGTH_SHORT).show();
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    if (result.getData() != null) {
+                        image = result.getData().getData();
+                        uploadImage.setEnabled(true);
+                        Glide.with(getApplicationContext()).load(image).into(imageView);
                     }
+                } else {
+                    Toast.makeText(UploadPic.this, "Please select an image", Toast.LENGTH_SHORT).show();
                 }
             });
 
@@ -68,35 +70,35 @@ public class UploadPic extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        name = findViewById(R.id.input_name);
+        description = findViewById(R.id.input_des);
+        point = findViewById(R.id.input_point);
+
         progress = findViewById(R.id.progress);
         imageView = findViewById(R.id.imageView);
         selectImage = findViewById(R.id.selectImage);
         uploadImage = findViewById(R.id.uploadImage);
 
-
-        selectImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                activityResultLauncher.launch(intent);
-            }
+        selectImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            activityResultLauncher.launch(intent);
         });
 
-        uploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage(image);
-            }
+        uploadImage.setOnClickListener(v -> {
+            // Move the EditText value assignments here to get the latest values
+            name1 = name.getText().toString().trim();
+            description1 = description.getText().toString().trim();
+            point1 = point.getText().toString().trim();
+
+            uploadImage(image);
         });
     }
 
     private void uploadImage(Uri image) {
         StorageReference reference = storageReference.child("images/" + UUID.randomUUID().toString());
         reference.putFile(image).addOnSuccessListener(taskSnapshot -> {
-            // Get the download URL
             reference.getDownloadUrl().addOnSuccessListener(downloadUri -> {
-                // Save the download URL to the Realtime Database
                 saveImageUrlToDatabase(Uri.parse(downloadUri.toString()));
             });
 
@@ -111,12 +113,33 @@ public class UploadPic extends AppCompatActivity {
         DatabaseReference databaseReference = database.getReference("Reward");
         String imageKey = databaseReference.push().getKey();
 
-        databaseReference.child(imageKey).setValue(imageUrl)
-                .addOnSuccessListener(aVoid -> {
-                    Toast.makeText(UploadPic.this, "Image URL saved to database", Toast.LENGTH_SHORT).show();
+        // Use the existing storageReference directly
+        StorageReference fileReference = storageReference.child("images/" + imageKey + ".jpg");
+
+        fileReference.putFile(imageUrl)
+                .addOnSuccessListener(taskSnapshot -> {
+                    fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                        // Create a new RewardHelperClass instance
+                        RewardHelperClass helperClass = new RewardHelperClass(name1, description1, point1);
+
+                        // Set the properties individually under the imageKey node
+                        databaseReference.child(imageKey).child("profileImageUrl").setValue(uri.toString());
+                        databaseReference.child(imageKey).child("name").setValue(helperClass.getName());
+                        databaseReference.child(imageKey).child("description").setValue(helperClass.getDescription());
+                        databaseReference.child(imageKey).child("point").setValue(helperClass.getPoint());
+
+                        // Display a success message
+                        Toast.makeText(UploadPic.this, "Upload successful", Toast.LENGTH_LONG).show();
+
+                        // Load the image into the ImageView using Glide
+                        Glide.with(UploadPic.this).load(uri).into(imageView);
+                    });
                 })
                 .addOnFailureListener(e -> {
-                    Toast.makeText(UploadPic.this, "Failed to save image URL: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    // Handle the failure
+                    Toast.makeText(UploadPic.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
     }
+
+
 }
